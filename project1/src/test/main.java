f1606e9c761225b3b73e301f5290e80c1e4f12a1
kkb-98 <wkfrk1119@gmail.com>
2021-05-01 21:56:41 +0900

    add 이것은 테스트입니다.

--- project1/src/test/main.java
+++ project1/src/test/main.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
-/**
- *
- * @author nam-woogeun
- */
 public class main {
-    
+    public void main(String[] args){
+        System.out.println("이것은 테스트입니다");
+    }
 }
