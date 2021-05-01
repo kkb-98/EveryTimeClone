7ad992d92bee23c0b69273bb622d30908e7dfe60
kkb-98 <wkfrk1119@gmail.com>
2021-05-01 23:12:52 +0900

    add 이것은 test_brunch를 테스팅하기 위한 테스트임.

--- EveryTime/src/main/main.java
+++ EveryTime/src/main/main.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-/**
- *
- * @author nam-woogeun
- */
+
 public class main {
+    public void main(String[] args){
+        System.out.println("이것은 test_brunch를 연습하기 위한 테스팅입니다.");
+    }
     
 }
