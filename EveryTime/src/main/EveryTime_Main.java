8893439316a3dacc4807372d93e28ee5711b640c
kkb-98 <wkfrk1119@gmail.com>
2021-05-02 02:27:50 +0900

    add start main.java-kkb

---
+++ EveryTime/src/main/EveryTime_Main.java
@@ -0,0 +1,10 @@
+package main;
+public class EveryTime_Main {
+
+    public static void main(String[] args) {
+        Grades_Function GF = new Grades_Function();
+        GF.sub_input();
+        
+    }
+    
+}
