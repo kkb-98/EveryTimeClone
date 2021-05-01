ffabc9089e305d222eab109f189baadb491a398d
WooGeun-Nam <rolru7686@gmail.com>
2021-05-01 21:21:44 +0900

    add

--- kkb_test/src/main/java/cse/kkb/kkb_test/kkb.java
+++ kkb_test/src/main/java/cse/kkb/kkb_test/kkb.java
@@ -9,6 +9,8 @@ public class kkb {
         String a = "저는 경빈ㅇ비니다.";
         System.out.println(a);
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        // vscode 로 주석 추가
+        System.out.println("추가추가"); // add
     }
     
 }
