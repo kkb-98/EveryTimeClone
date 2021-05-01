9fe5c402d988c562036cd83425c54c800ac46854
kkb-98 <wkfrk1119@gmail.com>
2021-05-01 21:07:07 +0900

    add string aaaaa

--- kkb_test/src/main/java/cse/kkb/kkb_test/kkb.java
+++ kkb_test/src/main/java/cse/kkb/kkb_test/kkb.java
@@ -8,6 +8,7 @@ public class kkb {
     public static void main(String[] args) {
         String a = "저는 경빈ㅇ비니다.";
         System.out.println(a);
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
     
 }
