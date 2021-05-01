5a8a5ad565a066a90719aedf41d9a22dc01d8647
kkb-98 <wkfrk1119@gmail.com>
2021-05-01 20:12:02 +0900

    add kkb_test_code[kkb]

---
+++ kkb_test/src/main/java/cse/kkb/kkb_test/kkb.java
@@ -0,0 +1,13 @@
+
+package cse.kkb.kkb_test;
+
+
+public class kkb {
+
+
+    public static void main(String[] args) {
+        String a = "저는 경빈ㅇ비니다.";
+        System.out.println(a);
+    }
+    
+}
