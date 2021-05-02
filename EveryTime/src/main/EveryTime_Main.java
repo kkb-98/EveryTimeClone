f8b8e64cbf5169638d950ddeb55b650df0192aa0
kkb-98 <wkfrk1119@gmail.com>
2021-05-02 21:22:41 +0900

    add 학점관리기능 함수실행문

--- EveryTime/src/main/EveryTime_Main.java
+++ EveryTime/src/main/EveryTime_Main.java
@@ -3,7 +3,7 @@ public class EveryTime_Main {
 
     public static void main(String[] args) {
         Grades_Function GF = new Grades_Function();
-        GF.sub_input();
+        GF.sub_input(); //학점관리기능 함수 실행
         
     }
     
